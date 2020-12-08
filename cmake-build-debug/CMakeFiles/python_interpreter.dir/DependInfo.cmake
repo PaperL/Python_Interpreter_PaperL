c10e0d8456b6cce44caf2cf77c5dba32389d089d
PaperL <982713785@qq.com>
2020-12-08 15:11:37 +0800

    更新忽略规则 不忽略/cmake-build-debug/CMakeFiles

---
+++ cmake-build-debug/CMakeFiles/python_interpreter.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "F:/WorkSpace/Python-Assignment-2020/generated/Python3BaseListener.cpp" "F:/WorkSpace/Python-Assignment-2020/cmake-build-debug/CMakeFiles/python_interpreter.dir/generated/Python3BaseListener.cpp.obj"
+  "F:/WorkSpace/Python-Assignment-2020/generated/Python3Lexer.cpp" "F:/WorkSpace/Python-Assignment-2020/cmake-build-debug/CMakeFiles/python_interpreter.dir/generated/Python3Lexer.cpp.obj"
+  "F:/WorkSpace/Python-Assignment-2020/generated/Python3Listener.cpp" "F:/WorkSpace/Python-Assignment-2020/cmake-build-debug/CMakeFiles/python_interpreter.dir/generated/Python3Listener.cpp.obj"
+  "F:/WorkSpace/Python-Assignment-2020/generated/Python3Parser.cpp" "F:/WorkSpace/Python-Assignment-2020/cmake-build-debug/CMakeFiles/python_interpreter.dir/generated/Python3Parser.cpp.obj"
+  "F:/WorkSpace/Python-Assignment-2020/src/main.cpp" "F:/WorkSpace/Python-Assignment-2020/cmake-build-debug/CMakeFiles/python_interpreter.dir/src/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../generated"
+  "../third_party"
+  "../third_party/runtime/src"
+  "../src"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "F:/WorkSpace/Python-Assignment-2020/cmake-build-debug/CMakeFiles/antlr4-cpp-runtime.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/python_interpreter.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/python_interpreter.dir/generated/Python3BaseListener.cpp.obj"
+  "CMakeFiles/python_interpreter.dir/generated/Python3Lexer.cpp.obj"
+  "CMakeFiles/python_interpreter.dir/generated/Python3Listener.cpp.obj"
+  "CMakeFiles/python_interpreter.dir/generated/Python3Parser.cpp.obj"
+  "CMakeFiles/python_interpreter.dir/src/main.cpp.obj"
+  "libpython_interpreter.dll.a"
+  "python_interpreter.exe"
+  "python_interpreter.exe.manifest"
+  "python_interpreter.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/python_interpreter.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
