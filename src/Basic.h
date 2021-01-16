927b7c15ae1f1bb5cbab602e73a9d33afd19f502
PaperL_Desktop <982713785@qq.com>
2021-01-16 23:10:03 +0800

    整合调整文件

--- src/Basic.h
+++ src/Basic.h
@@ -1,16 +1,36 @@
 //
-// Created by PaperL on 2020/12/10.
+// Created by PaperL on 2021/1/5.
 //
 
-#ifndef PYTHON_INTERPRETER_BASICVARIABLE_H
-#define PYTHON_INTERPRETER_BASICVARIABLE_H
+#ifndef PYTHON_INTERPRETER_BASIC_H
+#define PYTHON_INTERPRETER_BASIC_H
+
+
 
 #include <iostream>
+
+#include "robust.h"
+
+//----------BasicVariable----------
 #include <string>
 #include <sstream>//double to string
 
 #include "HighPrecision.h"
-#include "robust.h"
+
+//-----------pyNamespace-----------
+#include <map>
+#include <stack>
+
+#include "Python3BaseVisitor.h"
+
+typedef std::map<std::string, BasicVariable> variableMap;
+typedef std::map<std::string, Python3Parser::FuncdefContext *> functionMap;
+typedef std::stack<variableMap> variableStack;
+
+
+//=================================
+//========  BasicVariable  ========
+//=================================
 
 class BasicVariable {
 public:
@@ -139,4 +159,61 @@ public:
 };
 
 
-#endif //PYTHON_INTERPRETER_BASICVARIABLE_H
+
+//=================================
+//=========  pyNamespace  =========
+//=================================
+
+class pyNamespace {
+private:
+    //functionMap stdFunction;
+    functionMap userFunction;
+    variableMap globalVariable;
+    variableStack localVariableStack;
+public:
+
+    enum declareType {
+        pyNotDeclare, pyGlobal, pyLocal
+    };
+
+    BasicVariable getVariable(const std::string &name);
+
+
+    void assignVariable(const std::string &name, const BasicVariable &arg, declareType type = pyNotDeclare);
+    //赋值
+
+    BasicVariable getValue(const BasicVariable &arg);
+
+
+    void defineFunction(const std::string &name, Python3Parser::FuncdefContext *arg);
+
+    Python3Parser::FuncdefContext *getFunction(const std::string &name);
+};
+
+
+
+//=================================
+//===========  pyFlow  ============
+//=================================
+
+class pyFlow {
+public:
+    enum flowType {
+        pyBreak, pyContinue, pyReturn
+    };
+
+private:
+    flowType myType;
+    BasicVariable *returnValue;
+
+public:
+    explicit pyFlow(flowType type);
+
+    explicit pyFlow(flowType type,const BasicVariable &arg);
+
+    ~pyFlow();
+};
+
+
+
+#endif //PYTHON_INTERPRETER_BASIC_H
